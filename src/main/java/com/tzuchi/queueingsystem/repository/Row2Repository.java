e005e09c630c62fc4e2337edb58bade887e49fef
Shake <zonglin1010@gmail.com>
2024-10-13 13:28:58 +0800

    corrected the logic of row 2, P A W are independent valuse so it will be assigned to distinct numbers

--- src/main/java/com/tzuchi/queueingsystem/repository/Row2Repository.java
+++ src/main/java/com/tzuchi/queueingsystem/repository/Row2Repository.java
@@ -3,12 +3,13 @@ package com.tzuchi.queueingsystem.repository;
 import com.tzuchi.queueingsystem.entity.Row2;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Row2Repository extends JpaRepository<Row2, String> {
     Row2 findFirstByInQueueTrueOrderByPriorityDescPatientNumberAsc();
 
-    @Query("SELECT MAX(r.patientNumber) FROM Row2 r")
-    Integer findMaxPatientNumber();
+    @Query("SELECT MAX(r.patientNumber) FROM Row2 r WHERE r.patientCategory = :category")
+    Integer findMaxPatientNumberByCategory(@Param("category") char category);
 }
